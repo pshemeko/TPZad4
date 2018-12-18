2071077fa3b451124c7a3d4c5cc377b79e4858fd
przemo <p.otrembski@wp.pl>
2018-12-19 00:36:17 +0100

    zaczalem dobra wersje

---
+++ Zad4/WarstwaUslug/RepozytoriumDanych.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarstwaUslug
+{
+    public class RepozytoriumDanych
+    {
+        private static BazaDanychDataContext dataContex = new BazaDanychDataContext();
+
+        public static BazaDanychDataContext DataContex
+        {
+            get => dataContex;
+            set => dataContex = value;
+        }
+
+        public static void StworzWyporzyczenie(Wypozyczenia wyp)
+        {
+            dataContex.Wypozyczenia.InsertOnSubmit(wyp);
+            try
+            {
+                dataContex.SubmitChanges();
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+
+        public static void StworzCzytelnika(Czytelnicy czyt)
+        {
+            dataContex.Czytelnicy.InsertOnSubmit(czyt);
+            try
+            {
+                DataContex.SubmitChanges();
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+
+        //TODO DOKONCZYC
+        // get all, delete update
+
+    }
+}
