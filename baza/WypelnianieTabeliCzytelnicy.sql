326c82c9efe5b0353b0ad72ea510f24b06dc9a1c
przemo <p.otrembski@wp.pl>
2019-01-06 14:35:48 +0100

    zmienilem pliki do tworzenia bazy

--- baza/WypelnianieTabeliCzytelnicy.sql
+++ baza/WypelnianieTabeliCzytelnicy.sql
@@ -1,19 +1,18 @@
-USE [AJPO]
+USE [AJPO2]
 GO
 
-INSERT INTO [dbo].[czytelnicy]
-           ([id_czytelnika]
-           ,[nazwisko]
-           ,[imie]
-           ,[pesel]
-           ,[plec]
-           ,[telefon])
+INSERT INTO [dbo].[Czytelnicy]
+           ([Nazwisko]
+           ,[Imie]
+           ,[Pesel]
+           ,[Plec]
+           ,[Telefon])
      VALUES
-			( 1 , 'Brzeczyszczykiewicz', 'Grzegorz' , '76030553421', 'm' ,'500-432-954'),
-			( 2 , 'Nowak', 'Jan', '20111234653', 'm', '678-654-234'),
-			( 3 , 'Zarobiona', 'Ilona', '90052287544', 'k', '800543765'),
-			( 4 , 'WajchePrzeloz', 'tadeusz', '67113056741', 'm', '634-435-655'),
-			( 5 , 'Zbidula', 'Urszula', '02020245654', 'k', '800400800')
+			( 'Brzeczyszczykiewicz', 'Grzegorz' , '76030553421', 'm' ,'500-432-954'),
+			( 'Nowak', 'Jan', '20111234653', 'm', '678-654-234'),
+			( 'Zarobiona', 'Ilona', '90052287544', 'k', '800543765'),
+			( 'WajchePrzeloz', 'tadeusz', '67113056741', 'm', '634-435-655'),
+			( 'Zbidula', 'Urszula', '02020245654', 'k', '800400800')
 
 GO
 
--- baza/WypelnianieTabeliWypozyczenia.sql
+++ baza/WypelnianieTabeliWypozyczenia.sql
@@ -1,20 +1,19 @@
-USE [AJPO]
+USE [AJPO2]
 GO
 
-INSERT INTO [dbo].[wypozyczenia]
-           ([id_w]
-           ,[sygnatura]
-           ,[id_czytelnika]
-           ,[tytul_ksiazki]
-           ,[autor]
-           ,[gatunek]
-           ,[kara])
+INSERT INTO [dbo].[Wypozyczenia]
+           ([ID_czytelnika]
+		   ,[Sygnatura]
+		   ,[Tytul_ksiazki]
+		   ,[Autor]
+		   ,[Gatunek]
+		   ,[Kara])
      VALUES
-           (1 , 'ISBN 978-83-283-2446-6', 1 , 'C# leksykon wiedzy' ,'Joseph Albahari' , 'naukowa',0.0),
-		   (2 , 'ISBN 645-46-456-4655-0', 2 , 'Podroze pana kleka' , 'Pan kleks', 'bajki' , 1.2),
-		   (3 , 'ISBN 978-83-283-2480-0', 1 , 'Jacva Podstawy' , 'Cay S. Horstman', 'naukowa',1.7),
-		   (4 , 'ISBN 965-43-432-4564-4', 4 , 'Hobbit czyli tam i spowrotem', 'Tolkien', 'legendy', 0.0),
-		   (5 , 'ISBN 978-83-204-3472-9', 2 , 'Inzynieria oprogramowania. Wzorce projektowe' , 'Erich Gamma', 'naukowa', 4.6)
+           (1 ,'ISBN 978-83-283-2446-6', 'C# leksykon wiedzy' ,'Joseph Albahari' , 'naukowa',0.0),
+		   (2 ,'ISBN 645-46-456-4655-0', 'Podroze pana kleka' , 'Pan kleks', 'bajki' , 1.2),
+		   (1 ,'ISBN 978-83-283-2480-0', 'Java Podstawy' , 'Cay S. Horstman', 'naukowa',1.7),
+		   (4 ,'ISBN 965-43-432-4564-4', 'Hobbit, tam i spowrotem', 'Tolkien', 'legendy', 0.0),
+		   (2 ,'ISBN 978-83-204-3472-9', 'Inzynieria oprogramowania' , 'Erich Gamma', 'naukowa', 4.6)
 GO
 
 
